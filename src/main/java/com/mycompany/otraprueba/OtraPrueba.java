e436766261cbae27bdf7f8c5fd04993a8e656784
Honorato Saavedra Hernández <honoratosaavedra@gmail.com>
2024-11-07 08:14:41 -0600

    Esta es la versión inicial

---
+++ src/main/java/com/mycompany/otraprueba/OtraPrueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.otraprueba;
+
+/**
+ *
+ * @author honorato
+ */
+public class OtraPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
